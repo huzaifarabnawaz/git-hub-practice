cc45b958c1baff924f43dbeb5fbe521516c9aeb3
huzaifarabnawaz <160651248+huzaifarabnawaz@users.noreply.github.com>
2024-10-01 22:03:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app=express()
 
 console.log("chekin the page")
 console.log("new branch check")
-console.log("not stashing working ")
 
 app.listen(5000,()=>{
     console.log("post 5000 is runing ")
