5e22a07f0a5638c3f931adaf1806576b388a8c34
huzaifa <hrabnaw@gmail.com>
2024-10-01 21:15:12 +0500

    check commit

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app=express()
 
 console.log("chekin the page")
 console.log("new branch check")
+console.log("cheking the console.log")
 
 app.listen(5000,()=>{
     console.log("post 5000 is runing ")
-})
+})  
